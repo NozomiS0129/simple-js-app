8f52b5bd9771b0416fd50334e99e0d5978120960
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2023-06-27 20:22:42 -0400

    delete the alert and change content into pokemon list
    If applied, this commit will have deleted the alert 'Hello world' and change the content from favorite food into Pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'Icecream';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Jigglypuff', height: 0.5, weight: 5.5, type: ['fairy','normal']},
+    {name: 'Squirtle', height: 0.5, weight: 9.0, type: 'water'},
+    {name: 'Ditto', height: 0.3, weight: 4.0, type: 'normal'},
+    {name: 'Mew', height: 0.4, weight: 4.0, type: 'psychic'}
+];
