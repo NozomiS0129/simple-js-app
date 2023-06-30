d9d7fb9037877b4c1c518b181cf53bdb2047ae22
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2023-06-29 23:33:32 -0400

    add one element to pokemon list and line breaks
    If applied, this commit will have added one element to Pokemon list and  give line breaks.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,43 @@
 let pokemonList = [
-    {name: 'Jigglypuff', height: 0.5, weight: 5.5, type: ['fairy','normal']},
-    {name: 'Squirtle', height: 0.5, weight: 9.0, type: 'water'},
-    {name: 'Ditto', height: 0.3, weight: 4.0, type: 'normal'},
-    {name: 'Mew', height: 0.4, weight: 4.0, type: 'psychic'}
+    {
+        name: 'Jigglypuff',
+        height: 0.5,
+        weight: 5.5,
+        type: ['fairy','normal']
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        weight: 9.0,
+        type: 'water'
+    },
+    {
+        name: 'Ditto',
+        height: 0.3,
+        weight: 4.0,
+        type: 'normal'
+    },
+    {
+        name: 'Mew',
+        height: 0.4,
+        weight: 4.0,
+        type: 'psychic'
+    },
+    {
+        name: 'Onix',
+        height: 8.8,
+        weight: 210,
+        type: ['rock','ground']
+    }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let nameAndHeight = `${pokemonList[i].name}(height: ${pokemonList[i].height})`;
+
+    if(pokemonList[i].height > 8){
+        document.write(nameAndHeight + ' - Wow, that\'s big!');
+    }else{
+        document.write(nameAndHeight);
+    }
+    document.write('</br>');
+}
