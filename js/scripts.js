d1633168b511916fface4d58d0ab1cdbc9362224
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2023-07-05 18:03:45 -0400

    wrap pokemonList in an IIFE pokemonRepository
    If applied, this commit will have wrap the Pokemon list in an IIFE, which is pokemonRepository and add get all function and add function in it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,56 @@
-let pokemonList = [
-    {
-        name: 'Jigglypuff',
-        height: 0.5,
-        weight: 5.5,
-        type: ['fairy','normal']
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        weight: 9.0,
-        type: 'water'
-    },
-    {
-        name: 'Ditto',
-        height: 0.3,
-        weight: 4.0,
-        type: 'normal'
-    },
-    {
-        name: 'Mew',
-        height: 0.4,
-        weight: 4.0,
-        type: 'psychic'
-    },
-    {
-        name: 'Onix',
-        height: 8.8,
-        weight: 210,
-        type: ['rock','ground']
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Jigglypuff',
+            height: 0.5,
+            weight: 5.5,
+            type: ['fairy','normal']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            weight: 9.0,
+            type: 'water'
+        },
+        {
+            name: 'Ditto',
+            height: 0.3,
+            weight: 4.0,
+            type: 'normal'
+        },
+        {
+            name: 'Mew',
+            height: 0.4,
+            weight: 4.0,
+            type: 'psychic'
+        },
+        {
+            name: 'Onix',
+            height: 8.8,
+            weight: 210,
+            type: ['rock','ground']
+        }    
+    ];
 
-pokemonList.forEach(function(pokemon) {
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})()
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     let nameAndHeight = `${pokemon.name}(height: ${pokemon.height})`;
 
     if(pokemon.height > 8){
-        document.write(nameAndHeight + ' - Wow, that\'s big!');
+        document.write(`${nameAndHeight} - Wow, that\'s big!`);
     }else{
         document.write(nameAndHeight);
     }
